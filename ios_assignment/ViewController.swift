356a861af61964a58034f6389ffcbf9284fe4d49
Sai Naveen Katla <katlas.naveen@gmail.com>
2021-02-16 14:28:35 +0530

    Initial Commit

---
+++ ios_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_assignment
+//
+//  Created by Sai Naveen Katla on 16/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
